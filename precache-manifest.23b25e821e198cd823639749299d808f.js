332609e5f54ba2ce61418716023d6ae63add22b3
TRAORE Hervé <htraore1976@gmail.com>
2019-08-11 13:17:50 +0000

    initial version

---
+++ precache-manifest.23b25e821e198cd823639749299d808f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68573d65276aee362fd94e99434ce025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7648f3d76219c0575ecf",
+    "url": "/static/css/main.1d9971f9.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "7648f3d76219c0575ecf",
+    "url": "/static/js/main.81454043.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
